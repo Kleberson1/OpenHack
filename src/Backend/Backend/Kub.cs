08cbf0861b478fbbcac1efb04867402df5b13147
Fabricio Catae <fcatae@microsoft.com>
2018-08-30 08:58:10 -0300

    add yaml

--- src/Backend/Backend/Kub.cs
+++ src/Backend/Backend/Kub.cs
@@ -4,26 +4,60 @@ using System.Linq;
 using System.Threading.Tasks;
 using k8s;
 using k8s.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace Backend
 {
     public class Kub
     {
-        public static void Teste()
-        {
-            var yml = Yaml.LoadFromFileAsync<V1Pod>("nginx.yml").Result;
+        Kubernetes _client;
 
+        public Kub()
+        {
             var config = KubernetesClientConfiguration.BuildConfigFromConfigFile();
-            if( config.AccessToken == null )
+            if (config.AccessToken == null)
             {
                 config = new KubernetesClientConfiguration { Host = "http://127.0.0.1:8001" };
             }
 
-            var client = new Kubernetes(config);
+            _client = new Kubernetes(config);            
+        }
+
+        public V1PodList ListPods()
+        {
+            return _client.ListNamespacedPod("default", labelSelector: "tenant");
+        }
+
+        public V1ServiceList ListServices()
+        {
+            return _client.ListNamespacedService("default", labelSelector: "tenant");
+        }
+
+        public string CreatePod(string tenant, string filename)
+        {
+            var yml = Yaml.LoadFromFileAsync<V1Pod>(filename).Result;
+            yml.Metadata.Labels.Add("tenant", tenant);
+            var pod = _client.CreateNamespacedPod(yml, "default");
+
+            return pod.Metadata.Name;
+        }
+
+        public string CreateService(string tenant, string filename)
+        {
+            var yml = Yaml.LoadFromFileAsync<V1Service>(filename).Result;
+            yml.Spec.Selector.Add("tenant", tenant);
+            var svc = _client.CreateNamespacedService(yml, "default");
+            return svc.Metadata.Name;
+        }
+
+        public static void Teste()
+        {
+            var kub = new Kub();
 
             bool hasNginx = false;
 
-            var list = client.ListNamespacedPod("default");
+            var list = kub.ListPods();
+
             foreach (var item in list.Items)
             {
                 Console.WriteLine(item.Metadata.Name);
@@ -34,10 +68,10 @@ namespace Backend
                 }
             }
 
-            if(!hasNginx)
-            {
-                client.CreateNamespacedPod(yml, "default");
-            }
+            kub.CreatePod("t1", "pod.yml");
+            kub.CreateService("t1", "service.yml");
         }
+
+
     }
 }
